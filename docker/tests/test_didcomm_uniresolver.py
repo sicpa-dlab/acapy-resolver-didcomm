2d5f3c221dc245771d5dc56400d001bf2cd793ab
Daniel Bluhm <dbluhm@pm.me>
2021-04-01 14:15:30 -0400

    fix: integration test endpoint
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- docker/tests/test_didcomm_uniresolver.py
+++ docker/tests/test_didcomm_uniresolver.py
@@ -1,3 +1,7 @@
+"""Integration tests for DIDComm resolver."""
+
+# pylint: disable=redefined-outer-name
+
 import pytest
 import requests
 import time
@@ -12,14 +16,18 @@ REQUESTER = "http://requester:3001"
 
 
 def get(agent: str, path: str, **kwargs):
+    """Get."""
     return requests.get(f"{agent}{path}", **kwargs)
 
 
 def post(agent: str, path: str, **kwargs):
+    """Post."""
     return requests.post(f"{agent}{path}", **kwargs)
 
 
 def fail_if_not_ok(message: str):
+    """Fail the current test if wrapped call fails with message."""
+
     def _fail_if_not_ok(func):
         @wraps(func)
         def _wrapper(*args, **kwargs):
@@ -27,115 +35,119 @@ def fail_if_not_ok(message: str):
             if not response.ok:
                 pytest.fail(f"{message}: {response.content}")
             return response
+
         return _wrapper
+
     return _fail_if_not_ok
 
 
 def unwrap_json_response(func):
+    """Unwrap a requests response object to json."""
+
     @wraps(func)
     def _wrapper(*args, **kwargs):
         response = func(*args, **kwargs)
         return response.json()
+
     return _wrapper
 
 
 class Agent:
+    """Class for interacting with Agent over Admin API"""
+
     def __init__(self, url: str):
         self.url = url
 
     @unwrap_json_response
     @fail_if_not_ok("Create invitation failed")
     def create_invitation(self, **kwargs):
+        """Create invitation."""
         return post(self.url, "/connections/create-invitation", params=kwargs)
 
     @unwrap_json_response
     @fail_if_not_ok("Receive invitation failed")
     def receive_invite(self, invite: dict, **kwargs):
+        """Receive invitation."""
         return post(
-            self.url,
-            "/connections/receive-invitation",
-            params=kwargs,
-            json=invite
+            self.url, "/connections/receive-invitation", params=kwargs, json=invite
         )
 
     @unwrap_json_response
     @fail_if_not_ok("Accept invitation failed")
     def accept_invite(self, connection_id: str):
-        return get(
+        """Accept invitation."""
+        return post(
             self.url,
-            "/connections/accept-invitation",
-            params={"conn_id": connection_id}
+            f"/connections/{connection_id}/accept-invitation",
         )
 
     @unwrap_json_response
     @fail_if_not_ok("Failed to retreive connections")
     def retrieve_connections(self, connection_id: str = None, **kwargs):
+        """Retrieve connections."""
         return get(
             self.url,
             "/connections" if not connection_id else f"/connections/{connection_id}",
-            params=kwargs
+            params=kwargs,
+        )
+
+    @unwrap_json_response
+    @fail_if_not_ok("Failed to set connection metadata")
+    def metadata_set(self, connection_id, **metadata):
+        """Set connection metadata."""
+        return post(
+            self.url,
+            f"/connections/{connection_id}/metadata",
+            json={"metadata": metadata},
         )
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def requester():
+    """requester agent fixture."""
     yield Agent(REQUESTER)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def resolver():
+    """resolver agent fixture."""
     yield Agent(RESOLVER)
 
 
-def _requester_accept_invite(conn_id):
-    r = requests.get(
-        "http://requester:3001/connections/accept-invitation",
-        params={"conn_id": conn_id}
-    )
-    if not r.ok:
-        pytest.fail(f"connections invitation accept failed!{r.content}")
-    return r
-
-
-'''@pytest.fixture(scope="session", autouse=True)
-def establish_connection():
-    invite = _resolver_invitation(auto_accept="true").json()["invitation"]
-    r = _requester_receive_invite(invite,auto_accept="true")
-    conn_id = r.json()["connection_id"]
-    time.sleep(3)
-    yield conn_id '''
+@pytest.fixture(scope="session", autouse=True)
+def established_connection(resolver, requester):
+    """Established connection filter."""
+    invite = resolver.create_invitation(auto_accept="true")["invitation"]
+    resp = requester.receive_invite(invite, auto_accept="true")
+    yield resp["connection_id"]
 
 
 def test_conn_invitation(resolver):
+    """Test connection invitation."""
     resp = resolver.create_invitation(auto_accept="false")
     assert resp["invitation"]
     invite = resp["invitation"]
     assert invite["serviceEndpoint"]
     assert invite["recipientKeys"]
-    # Todo: check created connectiion
 
 
 def test_conn_receive_accept_invite(resolver, requester):
+    """Test connection receive accept invite."""
     invite = resolver.create_invitation(auto_accept="false")["invitation"]
-    #print(json.dumps(resolver.retrieve_connections(), indent=2))
-    received = requester.receive_invite(invite,auto_accept="false")
-    #print(json.dumps(received, indent=2))
-    #print(json.dumps(requester.retrieve_connections(), indent=2))
+    received = requester.receive_invite(invite, auto_accept="false")
     time.sleep(1)
     resp = requester.accept_invite(received["connection_id"])
-    # Todo: check created connectiion
+    assert resp
 
 
 def test_auto_accept_conn(resolver, requester):
+    """Test auto accepting connection."""
     invite = resolver.create_invitation(auto_accept="true")["invitation"]
-    received = requester.receive_invite(invite,auto_accept="true")
-    print(received)
-    # Todo: check created connectiion
+    received = requester.receive_invite(invite, auto_accept="true")
+    assert received
 
 
-#def test_pass(establish_connection):
-#    r = requests.get(f"http://requester:3001/resolver/resolve/{TEST_DID}")
-#    if r.ok:
-#        print(r.json())
-#    else:
-#        pytest.fail(f"resolver resolve failed!{r.content}")
+def test_no_resolver_connection_returns_error(established_connection):
+    """Test resolution over DIDComm Connection."""
+    resp = requests.get("http://requester:3001/resolver/resolve/did:example:123")
+    assert not resp.ok
