1be1b227ed3f4fe3fd663b457a924a48ba661252
Daniel Bluhm <dbluhm@pm.me>
2021-04-29 17:44:02 -0400

    refactor: simplify resolver tests
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,108 +1,49 @@
 """Test universal resolver with did-comm messaging."""
-import json
-import os
-from unittest.mock import MagicMock
-from asynctest import mock
+# pylint: disable=redefined-outer-name,protected-access
 
-import pytest
+from contextlib import contextmanager
+import os
+
+from aries_cloudagent.connections.models.conn_record import ConnRecord
+from aries_cloudagent.core.in_memory import InMemoryProfile
 from aries_cloudagent.messaging.request_context import RequestContext
+from aries_cloudagent.messaging.responder import BaseResponder, MockResponder
 from aries_cloudagent.resolver.base import (
-    ResolverError,
-    DIDNotFound,
     DIDMethodNotSupported,
+    DIDNotFound,
+    ResolverError,
 )
-from aries_cloudagent.storage.record import StorageRecord
-from asynctest import mock as async_mock
+from asynctest import mock
+import pytest
+import yaml
 
 from didcomm_resolver import DIDCommResolver
+from didcomm_resolver.acapy_tools.awaitable_handler import WaitingForMessageFailed
+from didcomm_resolver.protocol.v0_9 import ResolveDIDResult
+from didcomm_resolver.resolver import ResolverConnection
+from . import DOC
 
-from tests import DOC
 
-
-class AsyncMock(MagicMock):
-    async def __call__(self, *args, **kwargs):
-        return super(AsyncMock, self).__call__(*args, **kwargs)
+TEST_RESOLVER_CONNECTIONS = [ResolverConnection("test-1", {"test", "example"})]
 
 
 @pytest.fixture
 def resolver():
     """Resolver fixture."""
-    uni_resolver = DIDCommResolver()
-    uni_resolver.configure(
+    didcomm_resolver = DIDCommResolver()
+    didcomm_resolver.configure(
         {
-            "endpoint": "https://dev.uniresolver.io/1.0/identifiers",
-            "methods": [
-                "sov",
-                "abt",
-                "btcr",
-                "erc725",
-                "dom",
-                "stack",
-                "ethr",
-                "web",
-                "v1",
-                "key",
-                "ipid",
-                "jolo",
-                "hacera",
-                "elem",
-                "seraphid",
-                "github",
-                "ccp",
-                "work",
-                "ont",
-                "kilt",
-                "evan",
-                "echo",
-                "factom",
-                "dock",
-                "trust",
-                "io",
-                "bba",
-                "bid",
-                "schema",
-                "ion",
-                "ace",
-                "gatc",
-                "unisot",
-                "icon",
-            ],
+            "endpoint": "https://example.com",
+            "methods": ["example"],
         }
     )
-    yield uni_resolver
+    yield didcomm_resolver
 
 
 @pytest.fixture
 def profile():
     """Profile fixture."""
-    yield async_mock.MagicMock()
-
-
-class MockResponse:
-    """Mock didcomm response."""
-
-    pass
-
-
-class MockClientSession:
-    """Mock client session."""
-
-    def __init__(self, response: MockResponse = None):
-        self.response = response
-
-    def __call__(self):
-        return self
-
-    async def __aenter__(self):
-        """For use as async context."""
-        return self
-
-    async def __aexit__(self, err_type, err_value, err_exc):
-        """For use as async context."""
-
-    def get(self, endpoint):
-        """Return response."""
-        return self.response
+    yield InMemoryProfile.test_profile(bind={BaseResponder: MockResponder()})
 
 
 @pytest.fixture
@@ -110,50 +51,90 @@ def context():
     yield RequestContext.test_context()
 
 
-FAKE_YAML0 = "endpoint: magic\rmethods: test"
+@pytest.fixture(autouse=True)
+def mock_env():
+    with mock.patch.dict(os.environ, {"UNI_RESOLVER_CONFIG": "fake_config"}) as patched:
+        yield patched
 
 
-@async_mock.patch(
-    "builtins.open", new_callable=async_mock.mock_open, read_data=FAKE_YAML0
-)
+@pytest.fixture()
+def mock_config_file():
+    @contextmanager
+    def _mock_config_file(contents: str = None):
+        if not contents:
+            contents = yaml.dump(
+                {"endpoint": "http://example.com", "methods": ["example"]}
+            )
+
+        with mock.patch(
+            "builtins.open",
+            mock.mock_open(read_data=contents),
+        ) as patched:
+            yield patched
+
+    yield _mock_config_file
+
+
+@pytest.fixture(autouse=True)
+def mock_resolver_connections():
+    """Mock DIDCommResolver.resolver_connections."""
+    with mock.patch(
+        "didcomm_resolver.resolver.DIDCommResolver.resolver_connections",
+        mock.CoroutineMock(return_value=TEST_RESOLVER_CONNECTIONS),
+    ) as patched:
+        yield patched
+
+
+@pytest.fixture
+def response():
+    """Response message fixture."""
+    yield ResolveDIDResult(did_document=DOC)
+
+
+@pytest.fixture
+def conn_record():
+    """Connection record fixture."""
+    record = ConnRecord()
+    record.metadata_get = mock.CoroutineMock(return_value={"methods": "test"})
+    record.metadata_set = mock.CoroutineMock()
+    record.metadata_delete = mock.CoroutineMock()
+    yield record
+
+
+@pytest.fixture
+def MockConnRecord(conn_record):
+    """Mock ConnRecord fixture."""
+    with mock.patch("didcomm_resolver.resolver.ConnRecord") as patched:
+        patched.retrieve_by_id = mock.CoroutineMock(return_value=conn_record)
+        yield patched
+
+
 @pytest.mark.asyncio
-async def test_setup(mock_open, resolver, context):
-    with async_mock.patch.dict(os.environ, {"UNI_RESOLVER_CONFIG": "fake_config"}):
+async def test_setup(mock_config_file, resolver, context):
+    with mock_config_file():
         await resolver.setup(context)
-        assert resolver._supported_methods == "test"
-
-
-@async_mock.patch(
-    "builtins.open", new_callable=async_mock.mock_open, read_data=FAKE_YAML0
-)
-@async_mock.patch("yaml.load")
-@pytest.mark.asyncio
-async def test_setup_failed(yaml_load, mock_open, resolver, context):
-    yaml_load.return_value = "fail"
-    with async_mock.patch.dict(os.environ, {"UNI_RESOLVER_CONFIG": "fake_config"}):
-        with pytest.raises(ResolverError):
-            await resolver.setup(context)
+    assert resolver._supported_methods == ["example"]
 
 
 @pytest.mark.asyncio
-@async_mock.patch("os.environ")
-async def test_setup_env_error(env_mock, resolver, context):
-    env_mock.get.return_value = "error"
+async def test_setup_failed(mock_config_file, resolver, context):
+    with mock_config_file("fail"), pytest.raises(ResolverError):
+        await resolver.setup(context)
+
+
+@pytest.mark.asyncio
+async def test_setup_env_error(mock_env, resolver, context):
+    mock_env["DIDCOMM_RESOLVER_CONFIG"] = "error"
     with pytest.raises(ResolverError):
         await resolver.setup(context)
 
 
-FAKE_YAML1 = "NO_endpoint: magic\rNo_methodZ: test"
-
-
-@async_mock.patch(
-    "builtins.open", new_callable=async_mock.mock_open, read_data=FAKE_YAML1
-)
 @pytest.mark.asyncio
-async def test_setup_yaml_error(mock_open, resolver, context):
-    with async_mock.patch.dict(os.environ, {"UNI_RESOLVER_CONFIG": "fake_config"}):
-        with pytest.raises(ResolverError):
-            await resolver.setup(context)
+async def test_setup_yaml_error(mock_config_file, resolver, context):
+    with mock_config_file(
+        yaml.dump({"not_endpoint": "http://example.com", "not_methods": ["example"]})
+    ), pytest.raises(ResolverError):
+        await resolver.setup(context)
 
 
 @pytest.mark.asyncio
@@ -162,203 +143,60 @@ def test_supported_methods(resolver):
 
 
 @pytest.mark.asyncio
-def test_configre_error(resolver):
+def test_configure_error(resolver):
     with pytest.raises(ResolverError):
         resolver.configure({"fake": "configure"})
 
 
 @pytest.mark.asyncio
-@mock.patch("didcomm_resolver.resolver.send_and_wait_for_response")
-@mock.patch("didcomm_resolver.resolver.ResolveDIDResult")
-async def test_resolve_dict(ResolveDIDMock, send_wait_Mock, resolver, profile):
-    did_example = "did:sov:201029023831"
-    mock_inject = MagicMock()
-    mock_inject.inject.return_value = True
-
-    records = AsyncMock()
-    records.find_all_records.return_value = [
-        StorageRecord(
-            type="connection_metadata",
-            value={"methods": ["sov"]},
-            tags={
-                "key": "didcomm_uniresolver",
-                "connection_id": "1732d18d-c6f6-4e68-b3a7-56cc31d3313b",
-            },
-            id="5b9b78a061e6435bbbd7d5cde02d4192",
-        )
-    ]
-
-    def inject_aux(*args):
-        result = True
-        if str(args[0]).find("BaseResponder") < 0:
-            result = records
-        return result
-
-    profile.session.return_value.__aenter__.return_value.inject.side_effect = inject_aux
-
-    async def aux(*args, **kwargs):
-        mock = MagicMock()
-        mock.did_document = DOC
-        return mock
-
-    send_wait_Mock.side_effect = aux
-
-    result = await resolver.resolve(profile, did_example)
-
-    assert result.serialize() == DOC
+async def test_resolve_dict(resolver, profile, response):
+    with mock.patch(
+        "didcomm_resolver.resolver.send_and_wait_for_response",
+        mock.CoroutineMock(return_value=response),
+    ):
+        result = await resolver._resolve(profile, "did:example:1234abcd")
+        assert result == DOC
 
 
 @pytest.mark.asyncio
-@mock.patch("didcomm_resolver.resolver.send_and_wait_for_response")
-@mock.patch("didcomm_resolver.resolver.ResolveDIDResult")
-async def test_resolve_diddoc_json(ResolveDIDMock, send_wait_Mock, resolver, profile):
-    did_example = "did:sov:201029023831"
-    mock_inject = MagicMock()
-    mock_inject.inject.return_value = True
-
-    records = AsyncMock()
-    records.find_all_records.return_value = [
-        StorageRecord(
-            type="connection_metadata",
-            value={"methods": ["sov"]},
-            tags={
-                "key": "didcomm_uniresolver",
-                "connection_id": "1732d18d-c6f6-4e68-b3a7-56cc31d3313b",
-            },
-            id="5b9b78a061e6435bbbd7d5cde02d4192",
-        )
-    ]
-
-    def inject_aux(*args):
-        result = True
-        if str(args[0]).find("BaseResponder") < 0:
-            result = records
-        return result
-
-    profile.session.return_value.__aenter__.return_value.inject.side_effect = inject_aux
-
-    async def aux(*args, **kwargs):
-        mock = MagicMock()
-        mock.did_document = json.dumps(DOC)
-        return mock
-
-    send_wait_Mock.side_effect = aux
-
-    result = await resolver.resolve(profile, did_example)
-
-    assert result.serialize() == DOC
+async def test_resolve_not_found(resolver, profile):
+    with mock.patch(
+        "didcomm_resolver.resolver.send_and_wait_for_response",
+        mock.CoroutineMock(side_effect=DIDNotFound),
+    ), pytest.raises(DIDNotFound):
+        await resolver._resolve(profile, "did:example:1234abcd")
 
 
 @pytest.mark.asyncio
-@mock.patch("didcomm_resolver.resolver.send_and_wait_for_response")
-@mock.patch("didcomm_resolver.resolver.ResolveDIDResult")
-async def test_resolve_not_found(ResolveDIDMock, send_wait_Mock, resolver, profile):
-    did_example = "did:sov:201029023831"
-    mock_inject = MagicMock()
-    mock_inject.inject.return_value = True
-
-    records = AsyncMock()
-    records.find_all_records.return_value = [
-        StorageRecord(
-            type="connection_metadata",
-            value={"methods": ["sov"]},
-            tags={
-                "key": "didcomm_uniresolver",
-                "connection_id": "1732d18d-c6f6-4e68-b3a7-56cc31d3313b",
-            },
-            id="5b9b78a061e6435bbbd7d5cde02d4192",
-        )
-    ]
-
-    def inject_aux(*args):
-        result = True
-        if str(args[0]).find("BaseResponder") < 0:
-            result = records
-        return result
-
-    profile.session.return_value.__aenter__.return_value.inject.side_effect = inject_aux
-
-    async def aux(*args, **kwargs):
-        raise DIDNotFound()
-
-    send_wait_Mock.side_effect = aux
-
-    with pytest.raises(DIDNotFound):
-        await resolver.resolve(profile, did_example)
+async def test_resolve_timeout(resolver, profile):
+    with mock.patch(
+        "didcomm_resolver.resolver.send_and_wait_for_response",
+        mock.CoroutineMock(side_effect=WaitingForMessageFailed),
+    ), pytest.raises(DIDNotFound):
+        await resolver._resolve(profile, "did:example:1234abcd")
 
 
 @pytest.mark.asyncio
-@mock.patch("didcomm_resolver.resolver.send_and_wait_for_response")
-@mock.patch("didcomm_resolver.resolver.ResolveDIDResult")
-async def test_resolve_responder_fail(
-    ResolveDIDMock, send_wait_Mock, resolver, profile
-):
-    did_example = "did:sov:201029023831"
-    mock_inject = MagicMock()
-    mock_inject.inject.return_value = True
-
-    records = AsyncMock()
-    records.find_all_records.return_value = [
-        StorageRecord(
-            type="connection_metadata",
-            value={"methods": ["sov"]},
-            tags={
-                "key": "didcomm_uniresolver",
-                "connection_id": "1732d18d-c6f6-4e68-b3a7-56cc31d3313b",
-            },
-            id="5b9b78a061e6435bbbd7d5cde02d4192",
-        )
-    ]
-
-    def inject_aux(*args):
-        result = None
-        if str(args[0]).find("BaseResponder") < 0:
-            result = records
-        return result
-
-    profile.session.return_value.__aenter__.return_value.inject.side_effect = inject_aux
-
-    async def aux(*args, **kwargs):
-        raise DIDNotFound()
-
-    send_wait_Mock.side_effect = aux
-
-    with pytest.raises(ResolverError):
-        await resolver.resolve(profile, did_example)
+async def test_resolve_no_responder(resolver, profile):
+    profile.context.injector.bind_instance(BaseResponder, False)
+    with pytest.raises(ValueError):
+        await resolver._resolve(profile, "did:example:1234abcd")
 
 
 @pytest.mark.asyncio
-@mock.patch("didcomm_resolver.resolver.send_and_wait_for_response")
-@mock.patch("didcomm_resolver.resolver.ResolveDIDResult")
-async def test_resolve_not_supported(ResolveDIDMock, send_wait_Mock, resolver, profile):
-    did_example = "did:sov:201029023831"
-    mock_inject = MagicMock()
-    mock_inject.inject.return_value = True
-
-    records = AsyncMock()
-    records.find_all_records.return_value = [
-        StorageRecord(
-            type="connection_metadata",
-            value='{"methods": ["example"]}',
-            tags={
-                "key": "didcomm_uniresolver",
-                "connection_id": "1732d18d-c6f6-4e68-b3a7-56cc31d3313b",
-            },
-            id="5b9b78a061e6435bbbd7d5cde02d4192",
-        )
-    ]
-    profile.session.return_value.__aenter__.return_value.inject.return_value = records
-
+async def test_resolve_not_supported(resolver, profile, mock_resolver_connections):
+    mock_resolver_connections.return_value = []
     with pytest.raises(DIDMethodNotSupported):
-        await resolver.resolve(profile, did_example)
+        await resolver._resolve(profile, "did:example:1234abcd")
 
 
 @pytest.mark.asyncio
-@mock.patch("didcomm_resolver.resolver.ConnRecord.retrieve_by_id")
-async def test_register_connection(retrieve_by_id_mock, resolver, profile):
-    connection_id = "did:sov:201029023831"
-    methods = ["sov"]
-    conn_mock = AsyncMock()
-    conn_mock.metadata_get.return_value = {"methods": ["method2", "sov"]}
-    retrieve_by_id_mock.return_value = conn_mock
-    await DIDCommResolver.register_connection(profile, connection_id, methods)
+async def test_set_resolver_connection(profile, MockConnRecord):
+    conn_id = "test"
+    methods = {"example"}
+    async with profile.session() as session:
+        resolver_connection = await DIDCommResolver.set_resolver_connection(
+            session, conn_id, methods
+        )
+    assert resolver_connection.connection_id == conn_id
+    assert resolver_connection.methods == methods
