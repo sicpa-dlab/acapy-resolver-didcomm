50206e920d9c518834829a585bf9d58b284f6401
Daniel Bluhm <dbluhm@pm.me>
2021-07-12 21:35:20 -0400

    fix: tests to use new backchannel client
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- int/tests/conftest.py
+++ int/tests/conftest.py
@@ -2,40 +2,55 @@
 
 import os
 import time
+
+from acapy_backchannel import Client
+from acapy_backchannel.api.connection import create_invitation, receive_invitation
+from acapy_backchannel.api.didcomm_resolver import (
+    register_resolver_connection,
+    unset_resolver_connection,
+)
+from acapy_backchannel.models.connection_register_request import (
+    ConnectionRegisterRequest,
+)
+from acapy_backchannel.models.create_invitation_request import CreateInvitationRequest
+from acapy_backchannel.models.receive_invitation_request import ReceiveInvitationRequest
 import pytest
-
-from acapy_client import ApiClient, Configuration
-from acapy_client.apis import ConnectionApi, DidcommResolverApi, ResolverApi
-from acapy_client.models import ReceiveInvitationRequest
-
-
-class Agent:
-    def __init__(self, config: Configuration):
-        self.config = config
-        self.client = ApiClient(config)
-        self.didcomm_resolver = DidcommResolverApi(self.client)
-        self.connections = ConnectionApi(self.client)
-        self.resolver = ResolverApi(self.client)
 
 
 @pytest.fixture(scope="session")
 def requester():
     url = os.environ.get("REQUESTER", "http://requester:3001")
-    yield Agent(Configuration(host=url))
+    yield Client(base_url=url)
 
 
 @pytest.fixture(scope="session")
 def resolver():
     url = os.environ.get("RESOLVER", "http://resolver:3001")
-    yield Agent(Configuration(host=url))
+    yield Client(base_url=url)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def established_connection(resolver, requester):
-    """Established connection filter."""
-    invite = resolver.connections.create_invitation(auto_accept="true").invitation
-    resp = requester.connections.receive_invitation(
-        body=ReceiveInvitationRequest(**invite.to_dict()), auto_accept="true"
+def established_connection(resolver: Client, requester: Client):
+    """Establish connection fixture."""
+    invite = create_invitation.sync(
+        json_body=CreateInvitationRequest(), client=resolver, auto_accept="true"
+    ).invitation
+    resp = receive_invitation.sync(
+        client=requester,
+        auto_accept="true",
+        json_body=ReceiveInvitationRequest.from_dict(invite.to_dict()),
     )
     time.sleep(1)
-    yield resp["connection_id"]
+    yield resp.connection_id
+
+
+@pytest.fixture
+def resolver_connection(established_connection, requester: Client):
+    """Fixture for a registered resolver connection with cleanup."""
+    register_resolver_connection.sync(
+        client=requester,
+        conn_id=established_connection,
+        json_body=ConnectionRegisterRequest(methods=["mock"]),
+    )
+    yield
+    unset_resolver_connection.sync(client=requester, conn_id=established_connection)
--- int/tests/test_didcomm_connections.py
+++ int/tests/test_didcomm_connections.py
@@ -2,68 +2,75 @@
 
 # pylint: disable=redefined-outer-name, unused-argument
 
-from acapy_client.exceptions import ApiException
-from acapy_client.models import ConnectionRegisterRequest, ReceiveInvitationRequest
-import pytest
-
-from .conftest import Agent
-
-
-@pytest.fixture
-def resolver_connection(established_connection, requester: Agent):
-    """Fixture for a registered resolver connection with cleanup."""
-    requester.didcomm_resolver.register_resolver_connection(
-        established_connection, body=ConnectionRegisterRequest(methods=["test"])
-    )
-    yield
-    requester.didcomm_resolver.unset_resolver_connection(established_connection)
+from acapy_backchannel import Client
+from acapy_backchannel.api.didcomm_resolver import (
+    register_resolver_connection,
+    resolver_connections,
+    unset_resolver_connection,
+    resolver_connection as get_resolver_connection,
+)
+from acapy_backchannel.models.connection_register_request import (
+    ConnectionRegisterRequest,
+)
 
 
-def test_retrieve_zero_connections(requester: Agent):
+def test_retrieve_zero_connections(requester: Client):
     """Test retrieve DIDComm Connections."""
-    resp = requester.didcomm_resolver.resolver_connections()
-    assert len(resp["results"]) == 0
+    resp = resolver_connections.sync(client=requester)
+    assert len(resp.results) == 0
 
 
-def test_register_didcomm_connection(requester: Agent, resolver_connection):
+def test_register_didcomm_connection(requester: Client, resolver_connection):
     """Test retrieve DIDComm Connections."""
-    assert requester.didcomm_resolver.resolver_connections()["results"]
+    resp = resolver_connections.sync(client=requester)
+    assert resp.results
 
 
-def test_retrieve_connections(requester: Agent, resolver_connection):
+def test_retrieve_connections(requester: Client, resolver_connection):
     """Test retrieve DIDComm Connections."""
-    resp = requester.didcomm_resolver.resolver_connections()
-    assert len(resp["results"]) == 1
+    resp = resolver_connections.sync(client=requester)
+    assert len(resp.results) == 1
 
 
 def test_retrieve_specific_connection_by_id(
-    requester: Agent, established_connection, resolver_connection
+    requester: Client, established_connection, resolver_connection
 ):
     """Test retrieve DIDComm Connections by id."""
-    conn = requester.didcomm_resolver.resolver_connection(established_connection)
-    assert conn["connection_id"] == established_connection
-
-
-def test_fail_to_retrieve_no_existing_specific_connection_by_id(requester: Agent):
-    """Test to fail the retrieve DIDComm Connections by id."""
-    with pytest.raises(ApiException) as error:
-        requester.didcomm_resolver.resolver_connection("doesn't exist")
-        assert error.value.status == 400
-
-
-def test_unset_resolver_connection(established_connection, requester: Agent):
-    """Test remove DIDComm resolver Connection record."""
-
-    requester.didcomm_resolver.register_resolver_connection(
-        established_connection, body=ConnectionRegisterRequest(methods=["test"])
+    resp = get_resolver_connection.sync(
+        client=requester, conn_id=established_connection
     )
-    requester.didcomm_resolver.unset_resolver_connection(established_connection)
-    connections = requester.didcomm_resolver.resolver_connections()
-    assert len(connections["results"]) == 0
+    assert resp.connection_id == established_connection
 
 
-def test_fail_to_remove_no_existing_connection_record(requester: Agent):
+def test_fail_to_retrieve_no_existing_specific_connection_by_id(requester: Client):
+    """Test to fail the retrieve DIDComm Connections by id."""
+    resp = get_resolver_connection.sync_detailed(client=requester, conn_id="DNE")
+    assert resp.status_code == 400
+
+
+def test_unset_resolver_connection(established_connection, requester: Client):
+    """Test remove DIDComm resolver Connection record."""
+    register_resolver_connection.sync(
+        client=requester,
+        conn_id=established_connection,
+        json_body=ConnectionRegisterRequest(methods=["test"]),
+    )
+    unset_resolver_connection.sync(client=requester, conn_id=established_connection)
+    resp = resolver_connections.sync(client=requester)
+    assert len(resp.results) == 0
+
+
+def test_fail_to_remove_no_existing_connection_record(requester: Client):
     """Test to fail the remove DIDComm Connection record."""
-    with pytest.raises(ApiException) as error:
-        requester.didcomm_resolver.unset_resolver_connection("doesn't exist")
-        assert error.value.status == 400
+    resp = unset_resolver_connection.sync_detailed(client=requester, conn_id="DNE")
+    assert resp.status_code == 404
+
+
+def test_fail_register_method_to_not_existing_conn_id(requester: Client):
+    """Test to fail the register method over DIDComm Connection."""
+    resp = register_resolver_connection.sync_detailed(
+        client=requester,
+        conn_id="DNE",
+        json_body=ConnectionRegisterRequest(methods=["test"]),
+    )
+    assert resp.status_code != 200
